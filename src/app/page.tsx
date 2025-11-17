d9995cd4e46b51c05e3d49703ba62ef983f14616
Fadillatur Rizki <fadillaturrizki@gmail.com>
2025-11-17 23:13:50 +0000

    ini hapus ProTrade Terminal  termasuk icon logonya juga gua pengen supay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Header from '@/components/header';
 import MultiChartGrid from '@/components/multi-chart-grid';
 
 export default function Home() {
@@ -9,7 +8,6 @@ export default function Home() {
 
   return (
     <div className="flex flex-col min-h-screen bg-background">
-      <Header />
       <main className="flex-grow">
         <MultiChartGrid symbol={symbol} />
       </main>
