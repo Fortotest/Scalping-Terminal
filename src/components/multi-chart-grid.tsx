f23f189006e9909bcdcf7a8162cb0ec047f42747
Fadillatur Rizki <fadillaturrizki@gmail.com>
2025-11-18 00:53:00 +0000

    tampilkan h4 h1 m15 m5

--- src/components/multi-chart-grid.tsx
+++ src/components/multi-chart-grid.tsx
@@ -9,10 +9,10 @@ interface Props {
 
 export default function MultiChartGrid({ symbol, onSymbolChange }: Props) {
   const charts = [
-    { id: 'chart-d1', interval: 'D' },
     { id: 'chart-h4', interval: '240' },
     { id: 'chart-h1', interval: '60' },
     { id: 'chart-m15', interval: '15' },
+    { id: 'chart-m5', interval: '5' },
   ];
 
   return (
